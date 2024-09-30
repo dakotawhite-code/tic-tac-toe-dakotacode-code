df0290c04f301c8c0e12424eca4b5238d4fa64c6
dakotawhite-code <wd53169@mail.harpercollege.edu>
2024-09-29 20:56:32 -0500

    portfolio 1

--- lib/src/test/java/org/example/testtictactoe.java
+++ lib/src/test/java/org/example/testtictactoe.java
@@ -24,35 +24,35 @@ class testtictactoe {
 
     @Test
     void testcolumn() {
-        tictactoe.move(1); // X
-        tictactoe.move(2); // O
-        tictactoe.move(4); // X
-        tictactoe.move(5); // O
-        tictactoe.move(7); // X
-        assertTrue(tictactoe.win()); // X should win
+        tictactoe.move(1);
+        tictactoe.move(2);
+        tictactoe.move(4);
+        tictactoe.move(5);
+        tictactoe.move(7);
+        assertTrue(tictactoe.win());
     }
 
     @Test
     void testdiagonal() {
-        tictactoe.move(1); // X
-        tictactoe.move(2); // O
-        tictactoe.move(5); // X
-        tictactoe.move(3); // O
-        tictactoe.move(9); // X
-        assertTrue(tictactoe.win()); // X should win diagonally
+        tictactoe.move(1);
+        tictactoe.move(2);
+        tictactoe.move(5);
+        tictactoe.move(3);
+        tictactoe.move(9);
+        assertTrue(tictactoe.win());
     }
 
     @Test
     void testdraw() {
-        tictactoe.move(1); // X
-        tictactoe.move(2); // O
-        tictactoe.move(3); // X
-        tictactoe.move(4); // O
-        tictactoe.move(5); // X
-        tictactoe.move(6); // O
-        tictactoe.move(7); // X
-        tictactoe.move(8); // O
-        tictactoe.move(9); // X
-        assertTrue(tictactoe.draw()); // The tictactoe should end in a draw
+        tictactoe.move(1);
+        tictactoe.move(2);
+        tictactoe.move(3);
+        tictactoe.move(4);
+        tictactoe.move(5);
+        tictactoe.move(6);
+        tictactoe.move(7);
+        tictactoe.move(8);
+        tictactoe.move(9);
+        assertTrue(tictactoe.draw());
     }
 }
